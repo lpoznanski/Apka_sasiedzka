559df5176271715f0b601b399b9b35b53b4396d0
Lukasz Poznanski <l.poznanski3@gmail.com>
2023-03-21 13:59:28 +0100

    added Pillow to reqs, created Item model

--- apka/board/models.py
+++ apka/board/models.py
@@ -17,3 +17,14 @@ class Announcement(models.Model):
         self.slug = slugify(self.title)
         super(Announcement, self).save()
 
+class Item(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    photo = models.ImageField(upload_to='items')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
